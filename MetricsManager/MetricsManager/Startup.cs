7e02f2a2023da4b5f41edc024cce2e4478b63545
Pavel <paha-r@yandex.ru>
2021-04-26 14:27:18 +0300

    Add Crud

--- MetricsManager/MetricsManager/Startup.cs
+++ MetricsManager/MetricsManager/Startup.cs
@@ -24,6 +24,8 @@ namespace MetricsManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllers();
+            services.AddSingleton<ValuesHolder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ MetricsManager/MetricsManager/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MetricsManager
+{
+    public class WeatherForecast
+    {
+        public DataType Data { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string Summary { get; set; }
+    }
+}
